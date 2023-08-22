d91729ea10bc0ca0b8d6470db6aa9e2113e9bafd
danila <lysunckin.danil@yandex.ru>
2023-08-22 10:09:27 +0500

    moved project related methods to ProjectsController

--- src/main/java/com/project/resume/controllers/AdminController.java
+++ src/main/java/com/project/resume/controllers/AdminController.java
@@ -1,20 +1,17 @@
 package com.project.resume.controllers;
 
 import com.project.resume.model.Admin;
-import com.project.resume.model.Project;
 import com.project.resume.repo.ProjectRepository;
 import com.project.resume.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
-import java.util.Objects;
 
 @Controller
 @RequestMapping("/admin")
@@ -39,60 +36,7 @@ public class AdminController {
         List<Admin> adminList = (List<Admin>) userRepository.findAll();
         if (adminList.stream().anyMatch(x -> x.equals(admin))) {
             model.addAttribute("projects", projectRepository.findAll());
-            return "admin/admin_page";
-        } else return "redirect:/admin";
+            return "admin/index";
+        } else return "redirect:/admin/index";
     }
-
-    //Projects
-
-    @GetMapping("projects/add")
-    String addProject(Model model) {
-        model.addAttribute("project", new Project());
-        return "admin/project_form";
-    }
-
-    @PostMapping("projects/add")
-    String addProjectPost(@ModelAttribute Project project, @RequestParam(value = "file", required = false) MultipartFile file) {
-        Path dir = Paths.get("src/main/resources/templates/" + ProjectsController.PROJECT_CUSTOM_HTML);
-        Path filepath = Paths.get(dir.toString(), file.getOriginalFilename());
-        try {
-            file.transferTo(filepath);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        project.setFile_html(filepath.getFileName().toString());
-        projectRepository.save(project);
-        return "redirect:/admin";
-    }
-
-    @GetMapping("projects/{id}/edit")
-    String editProject(@PathVariable("id") int id, Model model) {
-        model.addAttribute("project", projectRepository.findById(id).orElse(null));
-        return "admin/project_edit_form";
-    }
-
-    @PostMapping("projects/{id}/edit")
-    String editProjectPost(@ModelAttribute Project project, @PathVariable("id") int id, @RequestParam(value = "file", required = false) MultipartFile file) {
-        if (projectRepository.findById(id).isPresent()) {
-            Project original_project = projectRepository.findById(id).get();
-            original_project.setTitle(project.getTitle());
-            original_project.setDescription(project.getDescription());
-            original_project.setMain(project.isMain());
-            original_project.setImg(project.getImg());
-            if (!Objects.requireNonNull(file.getOriginalFilename()).isEmpty()) {
-                addProjectPost(original_project, file);
-            } else {
-                projectRepository.save(original_project);
-            }
-        }
-
-        return "redirect:/admin";
-    }
-
-    @PostMapping("projects/{id}/delete")
-    String deleteProject(@PathVariable("id") int id) {
-        projectRepository.deleteById(id);
-        return "redirect:/admin";
-    }
-
 }
--- src/main/java/com/project/resume/controllers/ProjectsController.java
+++ src/main/java/com/project/resume/controllers/ProjectsController.java
@@ -5,14 +5,19 @@ import com.project.resume.repo.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.Objects;
 
 @Controller
+@RequestMapping("/projects")
 public class ProjectsController {
-    public static String PROJECT_CUSTOM_HTML = "projects/projects_custom_html/";
+    public static String PROJECT_CUSTOM_HTML = "projects/pages/";
     private final ProjectRepository projectRepository;
 
     @Autowired
@@ -20,16 +25,16 @@ public class ProjectsController {
         this.projectRepository = projectRepository;
     }
 
-    @GetMapping("/projects")
+    @GetMapping()
     public String projects(Model model) {
         List<Project> projects = (List<Project>) projectRepository.findAll();
         model.addAttribute("main_projects", projects.stream().filter(Project::isMain).toList());
         model.addAttribute("other_projects", projects.stream().filter(x -> !x.isMain()).toList());
         model.addAttribute("active", "projects");
-        return "projects/projects_page";
+        return "projects/projects";
     }
 
-    @GetMapping("/projects/{id}")
+    @GetMapping("/{id}")
     public String project(@PathVariable("id") int id, Model model) {
         model.addAttribute("active", "projects");
         if (projectRepository.findById(id).isPresent()) {
@@ -39,5 +44,54 @@ public class ProjectsController {
         return null;
     }
 
+    @GetMapping("/add")
+    String addProject(Model model) {
+        model.addAttribute("project", new Project());
+        return "projects/new_form";
+    }
+
+    @PostMapping("/add")
+    String addProjectPost(@ModelAttribute Project project, @RequestParam(value = "file", required = false) MultipartFile file) {
+        Path dir = Paths.get("src/main/resources/templates/" + ProjectsController.PROJECT_CUSTOM_HTML);
+        Path filepath = Paths.get(dir.toString(), file.getOriginalFilename());
+        try {
+            file.transferTo(filepath);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        project.setFile_html(filepath.getFileName().toString());
+        projectRepository.save(project);
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/{id}/edit")
+    String editProject(@PathVariable("id") int id, Model model) {
+        model.addAttribute("project", projectRepository.findById(id).orElse(null));
+        return "projects/edit_form";
+    }
+
+    @PostMapping("/{id}/edit")
+    String editProjectPost(@ModelAttribute Project project, @PathVariable("id") int id, @RequestParam(value = "file", required = false) MultipartFile file) {
+        if (projectRepository.findById(id).isPresent()) {
+            Project original_project = projectRepository.findById(id).get();
+            original_project.setTitle(project.getTitle());
+            original_project.setDescription(project.getDescription());
+            original_project.setMain(project.isMain());
+            original_project.setImg(project.getImg());
+            if (!Objects.requireNonNull(file.getOriginalFilename()).isEmpty()) {
+                addProjectPost(original_project, file);
+            } else {
+                projectRepository.save(original_project);
+            }
+        }
+
+        return "redirect:/admin";
+    }
+
+    @PostMapping("/{id}/delete")
+    String deleteProject(@PathVariable("id") int id) {
+        projectRepository.deleteById(id);
+        return "redirect:/admin";
+    }
 
 }
