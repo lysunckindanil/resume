836de9f28c69881d5e10bf09062f1b22254bdbe5
Danila <danil.lysunckin@gmail.com>
2024-11-15 23:29:43 +0500

    change logic in FilesService
    now added getPath method which takes Folder and due to folder returns String path
    String paths injected from property file

--- src/main/java/com/project/resume/service/FilesService.java
+++ src/main/java/com/project/resume/service/FilesService.java
@@ -10,16 +10,20 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Objects;
 
 @Service
 @Slf4j
 public class FilesService {
-    @Value("${resource-path}")
-    String resource_location_path;
+    @Value("${images-path}")
+    String images_path;
+    @Value("${projects-path}")
+    String projects_path;
+
 
     public String addFileToFolderStatic(MultipartFile file, Folder folder) {
         try {
-            file.transferTo(Path.of(resource_location_path + folder.toString().toLowerCase() + File.separator + file.getOriginalFilename()));
+            file.transferTo(Path.of(getPath(folder) + file.getOriginalFilename()));
         } catch (IOException e) {
             log.error(String.format("Unable to transfer %s to static folder %s", file.getOriginalFilename(), folder));
             log.error(e.toString());
@@ -29,7 +33,7 @@ public class FilesService {
 
     public void deleteFileFromStaticFolder(String file, Folder folder) {
         try {
-            Files.delete(Path.of(resource_location_path + folder.toString().toLowerCase() + File.separator + file));
+            Files.delete(Path.of(getPath(folder) + File.separator + file));
         } catch (IOException e) {
             log.error(String.format("Unable to delete %s from static folder %s", file, folder));
             log.error(e.toString());
@@ -37,6 +41,14 @@ public class FilesService {
     }
 
     public String[] getListOfFilesFromStaticDir(Folder folder) {
-        return new File(resource_location_path + folder.toString().toLowerCase()).list();
+        return new File(Objects.requireNonNull(getPath(folder))).list();
+    }
+
+    private String getPath(Folder folder) {
+        if (folder == Folder.IMAGES)
+            return images_path;
+        else if (folder == Folder.PROJECT_PAGES)
+            return projects_path;
+        return null;
     }
 }
