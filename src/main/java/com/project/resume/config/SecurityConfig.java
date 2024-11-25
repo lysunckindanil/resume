bd6ce19b778ce8b3769a26eb525ccba0224738a0
Danila <danil.lysunckin@gmail.com>
2024-11-25 13:46:04 +0500

    withdraw username and password to properties file

--- src/main/java/com/project/resume/config/SecurityConfig.java
+++ src/main/java/com/project/resume/config/SecurityConfig.java
@@ -1,12 +1,16 @@
 package com.project.resume.config;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -15,7 +19,19 @@ import javax.sql.DataSource;
 @Configuration
 @EnableWebSecurity
 @Slf4j
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    @Value("${auth.username}")
+    private String username;
+    @Value("${auth.password}")
+    private String password;
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 
     @Bean
     public SecurityFilterChain customSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -34,8 +50,8 @@ public class SecurityConfig {
     @Bean
     public JdbcUserDetailsManager users(DataSource dataSource) {
         UserDetails admin = User.builder()
-                .username("admin")
-                .password("{bcrypt}$2y$10$.bOnDBxF/BW5vub44x.zO.IQTzFarMiSrzWjoljmrSSfm3078xXZO")
+                .username(username)
+                .password(passwordEncoder().encode(password))
                 .roles("ADMIN")
                 .build();
         JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
